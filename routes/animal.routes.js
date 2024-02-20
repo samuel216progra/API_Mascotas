a6d8dd1e00c8949db640a78228b73796743d7cd8
samuel216progra <srodriguez-2022165@kinal.edu.gt>
2024-02-20 13:29:47 -0600

    se arreglo el post

--- routes/animal.routes.js
+++ routes/animal.routes.js
@@ -12,11 +12,11 @@ const router = Router();
 router.post(
     "/",
     [
-        check("nombre", "El nombre del animal es obligatorio").not().isEmpty(),
-        check("tipo", "El tipo del animal es obligatorio").not().isEmpty(),
-        check("raza", "La raza del animal es obligatorio").not().isEmpty(),
-        check("estadoDelAnimal", "El estado que se encuentra el animal es obligatorio").not().isEmpty(),
-        check("caracterizticas", "El estado que se encuentra el animal es obligatorio").not().isEmpty(),
+        check("nombre", "El nombre del animal es de caracter obligatorio obligatorio").not().isEmpty(),
+        check("tipo", "El tipo del animal es de caracter obligatorio").not().isEmpty(),
+        check("raza", "La raza del animal es de caracter obligatorio").not().isEmpty(),
+        check("estadoDelAnimal", "El estado que se encuentra el animal es de caracter obligatorioo").not().isEmpty(),
+        check("caracterizticas", "El estado que se encuentra el animal es de caracter obligatorio").not().isEmpty(),
         check("role").custom(esRolValido),
         validarCampos
     ], animalPost);
